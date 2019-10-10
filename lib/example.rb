1e21d2515e96f9ad3849733b974a4b607ccd6f8f
Ricky dildy <rdildy92@gmail.com>
2019-10-10 21:27:14 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,17 @@
 # Use this space to code along with the readme
-
+greeting = phrase
+def greeting 
 phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+
+end 
+$ ruby greeting.rb 
